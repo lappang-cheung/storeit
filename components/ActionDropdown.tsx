70aa2e785e6c498b1203a3b1bf6c4d4d11c74906
Leo Cheung <lappangcheung@gmail.com>
2024-11-13 00:41:07 -0500

    Setting up the action drop down menu

---
+++ components/ActionDropdown.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Models } from "node-appwrite";
+
+const ActionDropdown = ({ file }: { file: Models.Document }) => {
+  return <div>ActionDropdown</div>;
+};
+export default ActionDropdown;
