99853043ba959e3753b077dc457685c672e47458
Leo Cheung <lappangcheung@gmail.com>
2024-11-13 23:38:09 -0500

    Rename functionality implemented

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -26,6 +26,8 @@ import Link from "next/link";
 import { constructDownloadUrl } from "@/lib/utils";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { renameFile } from "@/lib/actions/file.actions";
+import { usePathname } from "next/navigation";
 
 const ActionDropdown = ({ file }: { file: Models.Document }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -34,6 +36,8 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
   const [action, setAction] = useState<ActionType | null>(null);
   const [name, setName] = useState(file.name);
   const [isLoading, setIsLoading] = useState(false);
+
+  const path = usePathname();
 
   const renderDialogContent = () => {
     if (!action) return null;
@@ -48,7 +52,28 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
       // setEmails to reset
     };
 
-    const handleAction = async () => {};
+    const handleAction = async () => {
+      if (!action) return;
+      setIsLoading(true);
+      let success = false;
+
+      const actions = {
+        rename: () =>
+          renameFile({
+            fileId: file.$id,
+            name,
+            extension: file.extension,
+            path,
+          }),
+        share: () => {},
+        delete: () => {},
+      };
+
+      success = await actions[action.value as keyof typeof actions]();
+
+      if (success) closeAllModals();
+      setIsLoading(false);
+    };
 
     return (
       <DialogContent className="shad-dialog button">
