07a4e721dbbb0e2b3ff7131e372b5e3e9308689b
Leo Cheung <lappangcheung@gmail.com>
2024-11-13 21:51:29 -0500

    Setting up the dialog and dropdown

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
+
+import { Dialog } from "@/components/ui/dialog";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 import { Models } from "node-appwrite";
 
 const ActionDropdown = ({ file }: { file: Models.Document }) => {
-  return <div>ActionDropdown</div>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+      <DropdownMenu open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </Dialog>
+  );
 };
 export default ActionDropdown;
--- components/Card.tsx
+++ components/Card.tsx
@@ -9,7 +9,7 @@ import ActionDropdown from "@/components/ActionDropdown";
 const Card = ({ file }: { file: Models.Document }) => {
   return (
     <Link href={file.url} target="_blank" className="file-card">
-      <div className="flex justifty-between">
+      <div className="flex justify-between">
         <Thumbnail
           type={file.type}
           extension={file.extension}
@@ -17,11 +17,13 @@ const Card = ({ file }: { file: Models.Document }) => {
           className="!size-20"
           imageClassName="!size-11"
         />
+
         <div className="flex flex-col items-end justify-between">
           <ActionDropdown file={file} />
           <p className="body-1">{convertFileSize(file.size)}</p>
         </div>
       </div>
+
       <div className="file-card-details">
         <p className="subtitle-2 line-clamp-1">{file.name}</p>
         <FormattedDateTime
