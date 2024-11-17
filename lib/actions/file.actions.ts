27c348fdc39fcf080e0e90f9ce8fdfc6b840eb93
Leo Cheung <lappangcheung@gmail.com>
2024-11-16 19:46:25 -0500

    Added the sorting for items to be display on the pages

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -61,18 +61,22 @@ export const uploadFile = async ({
   }
 };
 
-const createQueries = (currentUser: Models.Document) => {
+const createQueries = (currentUser: Models.Document, types: string[]) => {
   const queries = [
     Query.or([
       Query.equal("owner", currentUser.$id),
       Query.contains("users", currentUser.email),
     ]),
   ];
-  // TODO: Search, sort, limits...
+
+  if (types.length > 0) {
+    queries.push(Query.equal("type", types));
+  }
+
   return queries;
 };
 
-export const getFiles = async () => {
+export const getFiles = async ({ types = [] }: GetFilesProps) => {
   const { databases } = await createAdminClient();
 
   try {
@@ -80,7 +84,7 @@ export const getFiles = async () => {
 
     if (!currentUser) throw new Error("User not found");
 
-    const queries = createQueries(currentUser);
+    const queries = createQueries(currentUser, types);
 
     const files = await databases.listDocuments(
       appwriteConfig.databaseId,
