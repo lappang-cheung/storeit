abe469c9d09589529a7793e8fac46ae61c798842
Leo Cheung <lappangcheung@gmail.com>
2024-11-16 20:22:37 -0500

    Adding the search menu

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,11 +1,35 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
+import { useSearchParams } from "next/navigation";
+import { getFiles } from "@/lib/actions/file.actions";
+import { Models } from "node-appwrite";
 
 const Search = () => {
   const [query, setQuery] = useState("");
+  const [results, setResults] = useState<Models.Document[]>([]);
+  const [open, setOpen] = useState(false);
+
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get("query") || "";
+
+  useEffect(() => {
+    const fetchFile = async () => {
+      const files = await getFiles({ searchText: query });
+      setResults(files.document);
+      setOpen(true);
+    };
+
+    fetchFile();
+  }, []);
+
+  useEffect(() => {
+    if (searchQuery) {
+      setQuery("");
+    }
+  }, [searchQuery]);
 
   return (
     <div className="search">
@@ -22,6 +46,7 @@ const Search = () => {
           className="search-input"
           onChange={(e) => setQuery(e.target.value)}
         />
+        {open && <ul className="search-result">RESULTS</ul>}
       </div>
     </div>
   );
